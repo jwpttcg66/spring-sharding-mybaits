fc3b2740627c27348a18a520736d9760140942ec
jiangwenping <jwpttcg66>
2017-03-16 11:58:32 +0800

    增加分表sharding

--- src/test/java/com/snowcattle/demo/OrderServiceTest.java
+++ src/test/java/com/snowcattle/demo/OrderServiceTest.java
@@ -1,6 +1,5 @@
 package com.snowcattle.demo;
 
-import com.snowcattle.demo.entity.Order;
 import com.snowcattle.demo.service.impl.OrderService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -20,13 +19,13 @@ public class OrderServiceTest {
 
     @Test
     public void test(){
-        for(int i = 0; i < 12; i++){
-            Order order = new Order();
-            order.setUserId(i);
-            order.setOrderId(i);
-            order.setStatus("测试插入");
-            orderService.insertOrder(order);
-        }
+//        for(int i = 0; i < 12; i++){
+//            Order order = new Order();
+//            order.setUserId(i);
+//            order.setOrderId(i);
+//            order.setStatus("测试插入");
+//            orderService.insertOrder(order);
+//        }
 
     }
 }
